d12aefeee49495d2ad8cfac719972887517cc561
amir <amirhosseinsedaghati42@gmail.com>
2023-10-08 17:44:48 -0400

    add __init__.py

---
+++ flight_dashboard/__init__.py
@@ -0,0 +1,6 @@
+from . import the_distribution_of_arrival_delay
+from . import the_variability_of_arrival_delay
+from . import search_us_airline_carrier_info
+from . import departure_time_summary
+from . import arrival_time_summary
+from . import inference
